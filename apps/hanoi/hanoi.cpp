520fd2b76631c853ba1620a44b213e78a0193357
dagonzalez377-collab <dagonzalez377@gmail.com>
2026-05-27 21:56:18 -0500

    hw01: added chrono timing to recursive and iterative hanoi

--- apps/hanoi/hanoi.cpp
+++ apps/hanoi/hanoi.cpp
@@ -6,30 +6,25 @@ void moveDisks(int, int, int, int);
 void printIt(int , int , int );
 
 int main() {
-    const int FROMPEG = 1;
-    const int TOPEG = 3;
-    const int TEMPPEG = 2;
-    const int NUMDISKS = 3;
-    for (auto numdisks: {3,5,6}) {
-      cout << "Numdisks: " << numdisks << endl;
-      moveDisks(numdisks, FROMPEG, TOPEG, TEMPPEG);
-      cout << "Moved " << numdisks << " pegs"
-           << " from peg " << FROMPEG
-           << " to peg " << TOPEG << endl;
+    for (auto numdisks: {5, 10, 15, 20, 25, 30, 35}) {
+        auto start = chrono::steady_clock::now();
+        moveDisks(numdisks, 1, 3, 2);
+        auto end = chrono::steady_clock::now();
+
+        chrono::duration<double> elapsed = end - start;
+        cout << "numdisks: " << numdisks
+             << " time passed: " << scientific << elapsed.count() << "s" << endl;
     }
 }
 
 void moveDisks(int num, int fromPeg, int toPeg, int tempPeg) {
-     if (num > 0) {
-        moveDisks(num -1, fromPeg, tempPeg, toPeg);
+    if (num > 0) {
+        moveDisks(num-1, fromPeg, tempPeg, toPeg);
         printIt(num, fromPeg, toPeg);
         moveDisks(num-1, tempPeg, toPeg, fromPeg);
-     }
+    }
 }
 
 void printIt(int disk, int fromPeg, int toPeg) {
-  // Do nothing for timing test, but otherwise
-  cout<< "Move disk " << disk
-      << " from pef " << fromPeg
-      << " to peg " << toPeg << endl;
+    
 }
